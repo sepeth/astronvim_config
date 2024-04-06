2c490a990efebee65a5f89f1f9fa6f8d232e9648
Doğan Çeçen <sepeth@gmail.com>
2024-04-06 12:44:19 +0200

    Customize telescope to make C-n and C-p work like everywhere else

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,18 @@
+---@type LazySpec
+return {
+  "nvim-telescope/telescope.nvim",
+
+  -- the first parameter is the plugin specification
+  -- the second is the table of options as set up in Lazy with the `opts` key
+  config = function(plugin, opts)
+    local actions = require "telescope.actions"
+
+    -- run the core AstroNvim configuration function with the options table
+    require "astronvim.plugins.configs.telescope"(plugin, opts)
+
+    opts.defaults.mappings.i["<C-n>"] = actions.move_selection_next
+    opts.defaults.mappings.i["<C-p>"] = actions.move_selection_previous
+    opts.defaults.mappings.i["<C-j>"] = actions.cycle_history_next
+    opts.defaults.mappings.i["<C-k>"] = actions.cycle_history_prev
+  end,
+}
