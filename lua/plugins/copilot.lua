dc59ea1daa3494b031bb624a7dee4ee6f2b4e771
Doğan Çeçen <sepeth@hey.com>
2024-08-12 12:27:26 +0300

    Disable Copilot for Markdown
    It is annoying.

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -7,6 +7,10 @@ vim.keymap.set('i', '<C-e>', 'copilot#Accept("\\<CR>")', {
 
 vim.g.copilot_no_tab_map = true
 
+vim.g.copilot_filetypes = {
+  markdown = false,
+}
+
 ---@type LazySpec
 return {
   "github/copilot.vim",
