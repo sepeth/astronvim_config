b92c4fdaef84b8fd0d66704034045409452477e3
Doğan Çeçen <sepeth@hey.com>
2024-11-16 21:43:47 +0200

    Change toggling buffers and find buffer keymaps

--- lua/plugins/mappings.lua
+++ lua/plugins/mappings.lua
@@ -5,7 +5,8 @@ return {
     opts = {
       mappings = {
         n = {
-          ["<Leader><Leader>"] = { "<C-^>", desc = "Switch to last buffer" },
+          ["<Leader>bl"] = { "<C-^>", desc = "Switch to last buffer" },
+          ["<Leader><Leader>"] = { "<Leader>fb", desc = "Show buffers", remap = true },
           ["<D-k>"] = {
             ':lua require("dash").search(true, vim.fn.expand("<cword>"))<CR>',
             desc = "Find the word under the cursor in Dash",
